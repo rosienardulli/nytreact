4f248172784f60462e7fba097368e9a22e3a9f58
Rosie Nardulli <rosie.nardulli@gmail.com>
2018-06-30 13:50:44 -0500

    nyt hw

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import API from './../../utils/API';
-
+import Article from './../../Components/Article';
 
 class Home extends Component {
    state = {
@@ -12,6 +12,7 @@ class Home extends Component {
 
    componentDidMount() {
     this.getArticles();
+    this.getSavedArticles();
     }
 
    //my methods
@@ -30,12 +31,47 @@ class Home extends Component {
    
    .catch(err => console.log(err));
     }
+    handleArticleSave = id => {
+        const article = this.state.articles.find(article => article._id === id);
+        API.saveArticle(article).then(res => {
+            this.getSavedArticles();
+            this.getArticles()
+    });
+}
+
+    getSavedArticles = () => {
+        API.getSavedArticles()
+        .then(res => 
+        this.setState({
+            savedArticles: res.data
+        })
+    )
+        .catch(err => console.log(err));
+    }
+
    render() {
        return (
            <div>
                I am a list of articles
                {this.state.articles.map(article => (
-                   <div>{article.headline.main}</div>
+                   <Article
+                   key={article._id}
+                   _id={article._id}
+                   title={article.headline.main}
+                   url={article.web_url}
+                   date={article.pub_data}
+                   handleClick={this.handleArticleSave}
+                   buttonText="Save Article"
+                   />
+               ))}
+               <p> Saved Articles</p>
+               {this.state.articles.map(article => (
+                   <Article
+                   key={article._id}
+                   _id={article._id}
+                   title={article.headline.main}
+                   buttonText="Delete Article"
+                   />
                ))}
            </div>
        )
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,5 +5,14 @@ export default {
     //get articles from nyt
     getArticles: function(params) {
         return axios.get('/api/nyt', { params: filterParams(params) });
-    }
+    },
+
+    //save article to our db
+    saveArticle: function(articleData) {
+        return axios.post('/api/articles', articleData);
+    },
+
+    getSavedArticles: function() {
+    return axios.get('/api/articles');
+}
 };
